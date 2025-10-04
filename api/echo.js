59a4fd9d8d1357bd4caf0eb814f55785b77ab232
rogov-k <pluskika@gmail.com>
2025-10-04 05:11:17 +0300

    feat(echo): update eenpoint

--- api/echo.js
+++ api/echo.js
@@ -1,8 +1,5 @@
 export default function handler(req, res) {
-  if (req.method === "POST") {
-    res.status(200).json({ ok: true, received: req.body });
-  } else {
-    res.setHeader("Allow", ["POST"]);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+  if (req.method === "GET") {
+    res.status(200).json({ ok: true, hash: '12'});
   }
 }
