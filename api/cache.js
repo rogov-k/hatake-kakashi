d975d2c96ceec9bd97305af2a325173ddea6e3ae
rogov-k <pluskika@gmail.com>
2025-10-05 17:38:33 +0300

    feat: update .vercelignore

--- api/cache.js
+++ api/cache.js
@@ -1,4 +1,3 @@
-// api/cache.js
 export default async function handler(req, res) {
   const redisUrl = process.env.UPSTASH_REDIS_REST_URL;
   const redisToken = process.env.UPSTASH_REDIS_REST_TOKEN;
