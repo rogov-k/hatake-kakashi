0ca812fcb7213e8a642593bb7234d62de3adb1fb
rogov-k <pluskika@gmail.com>
2025-10-19 05:26:27 +0300

    chore: update deploy

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,12 @@
 #!/bin/zsh
 
 rm -rf dist
-npm run build && cp src/index.html dist/index.html
+
+npm run build
+cp src/index.html dist/index.html
+cp src/favicon.ico dist/favicon.ico
+cp src/favicon.svg dist/favicon.svg
+
 cd dist
 tree -a
 cd ../
